0e63375a8aa935845e0c153997718bc7873c9c2f
2cents <connortstokes@gmail.com>
2015-09-05 17:23:09 -0400

    Map updates

--- getit/settings.py
+++ getit/settings.py
@@ -128,3 +128,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
